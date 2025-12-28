53ec44874cc28f3a25ad47d29916cc4c2cf30033
budiart <ekobcode@gmail.com>
2025-12-29 03:05:30 +0700

    update

--- generateSignature.js
+++ generateSignature.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 
 const payload = JSON.stringify({ test: true });
-const secret = "super-secret";
+const secret = "WGvvrlLB1dP7DhCQ2rZTmR1LEtDSaOF7";
 
 const signature = crypto
   .createHmac("sha256", secret)
